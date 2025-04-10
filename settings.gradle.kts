7c67d8066128dc53208bd7638dbbdae5dd8f51e4
Anisha Bag <130827744+Anisha-Bag@users.noreply.github.com>
2025-04-10 12:09:12 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "P_5_MyRecipeApp"
+include(":app")
